cd34555adab3f72bdfc18550bc6450c0cf7e2b03
ninh <ninhnguyen375@gmail.com>
2019-01-31 14:41:59 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+const cors = require('cors');
+
+// config midleware 
+app.use(express.static('./build'));
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.sendFile('./build/index.html');
+});
+// Home page
+app.get('/api/home', (req, res) => {
+  res.json({title: 'Home'});
+});
+// about page
+app.get('/api/about', (req, res) => {
+  res.json({title: 'About'});
+});
+
+
+app.listen(PORT);
